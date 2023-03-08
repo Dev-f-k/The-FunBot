f2f3141b1af28be86e6c6c3df4d1b4973453bf11
Dev-f-k <112109531+Dev-f-k@users.noreply.github.com>
2023-03-08 09:44:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "The-Fun-Bot"},
+            plugins={"root": "plugins"},
             sleep_threshold=5,
         )
 
